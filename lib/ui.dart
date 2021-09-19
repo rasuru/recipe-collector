4051cab00ae95669dbe64b85df978dff45df6828
rasuru <rasuru@protonmail.com>
2021-09-19 15:41:04 +0600

    Add feedback to recipe deletion

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,23 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
-import 'package:recipe_collector/progress.dart';
-import 'package:recipe_collector/recipe_form/domain.dart';
-import 'package:recipe_collector/recipe_form/open_form/use_case.dart';
 import 'package:sliding_sheet/sliding_sheet.dart';
+import 'package:time/time.dart';
 
+import 'delete_recipe/state.dart';
+import 'progress.dart';
+import 'recipe_form/domain.dart';
 import 'recipe_form/open_form/state.dart';
+import 'recipe_form/open_form/use_case.dart';
 import 'recipe_form/providers.dart';
 import 'recipe_form/ui.dart';
 import 'recipe_list/providers.dart';
 import 'recipe_list/ui.dart';
 import 'ui/theme.dart';
+import 'ui/widgets/success_message.dart';
 
-class RecipeCollectorUI extends StatelessWidget {
+class RecipeCollectorUI extends StatefulWidget {
   RecipeCollectorUI({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<RecipeCollectorUI> createState() => _RecipeCollectorUIState();
+}
+
+class _RecipeCollectorUIState extends State<RecipeCollectorUI> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,6 +42,37 @@ class RecipeCollectorUI extends StatelessWidget {
         ),
       )),
     );
+  }
+
+  late final FToast fToast;
+  late final StreamSubscription deleteRecipeHandler;
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+    final deleteRecipeProgress$ = context.read<DeleteRecipeProgress$>();
+
+    deleteRecipeHandler = deleteRecipeProgress$.stream.listen((progress) {
+      if (progress.isCompleted) {
+        fToast.showToast(
+          gravity: ToastGravity.CENTER,
+          toastDuration: 1.seconds,
+          child: MultiProvider(
+            providers: [
+              Provider.value(value: context.read<UITheme>()),
+            ],
+            child: SuccessMessage(message: 'Recipe deleted'),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    deleteRecipeHandler.cancel();
+    super.dispose();
   }
 
   Widget buildScaffold(Widget child) {
@@ -65,27 +107,32 @@ class RecipeCollectorUI extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(20),
       child: BlocBuilder<OpenRecipeFormProgress$, Progress<EditedRecipe>>(
-        builder: (context, progress) {
-          return progress.fold(
-            ifIdle: () {
-              context.read<OpenRecipeFormUseCase>().emptyForm();
-              return Container();
-            },
-            ifActive: () => Center(child: CircularProgressIndicator()),
-            ifCompleted: (editedRecipe) {
-              return MultiProvider(
-                key: ValueKey(editedRecipe),
-                providers: createRecipeFormProviders(
-                  context: context,
-                  editedRecipe: editedRecipe,
-                ),
-                child: RecipeForm(),
-              );
-            },
-            ifFailed: (_) => Center(child: Text('Error')),
-          );
-        },
+        builder: buildRecipeFormLoader,
       ),
+    );
+  }
+
+  Widget buildRecipeFormLoader(
+    BuildContext context,
+    Progress<EditedRecipe> progress,
+  ) {
+    return progress.fold(
+      ifIdle: () {
+        context.read<OpenRecipeFormUseCase>().emptyForm();
+        return Container();
+      },
+      ifActive: () => Center(child: CircularProgressIndicator()),
+      ifCompleted: (editedRecipe) {
+        return MultiProvider(
+          key: ValueKey(editedRecipe),
+          providers: createRecipeFormProviders(
+            context: context,
+            editedRecipe: editedRecipe,
+          ),
+          child: RecipeForm(),
+        );
+      },
+      ifFailed: (_) => Center(child: Text('Error')),
     );
   }
 
